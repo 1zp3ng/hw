77f3eafd4cb1a4c073407feff2a54dda3a20c295
1zp3ng <102172166+1zp3ng@users.noreply.github.com>
2022-05-07 21:18:26 +0800

    Add files via upload

---
+++ hw_week8.cpp
@@ -0,0 +1,83 @@
+#include "std_lib_facilities.h"
+
+/*
+First part:  
+  4.   void print_array10(ostream& os, int* a) {
+       for (int i = 0; i < 10; ++i)
+           os << a[i] << "\n";
+       }
+  7.   void print_array(ostream& os, int* a, int n) {
+       for (int i = 0; i < n; ++i)
+          os << a[i] << "\n";
+       }
+  10.  void print_vector(ostream& os, vector<int> v) {
+       for (int i = 0; i < v.size(); ++i)
+          os << v[i] << "\n";
+       }
+*/
+int main()
+try{
+/*
+  1.   int* a1=new int[10];
+  2.   cout << *a1 << endl;
+  3.   double* calc(int res_size)
+       {
+          double* res = new double[res_size];
+          delete[] a1;
+          return res;
+       }
+  5.   int* a2=new int[10]{100,101,102,103,104,105,106,107,108,109};
+       cout << *a2 << endl;
+  6.   int* a3=new int[11]{100,101,102,103,104,105,106,107,108,109,110};
+       cout << *a3 << endl;
+  8/9. int* a4=new int[20]{100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120};
+       cout << *a4 << endl;
+       delete[] a4;
+       
+Second part:
+  1.   int a1 = 7;
+       int* p1 = &a1;  
+  2.   cout << "p1: " << p1 << endl;
+       cout << "*p1: " << *p1 << endl;
+  3.   int* p2 = new int[7];
+       for (int i = 0; i < 7; ++i)
+          p2[i] = pow(2, i);
+  4.   cout << "p2: " << p2 << endl;
+       cout << "print_array(cout,p2,7):\n";
+       print_array(cout, p2, 7);
+  5.   int* p3 = p2;
+  6/7. p1=p2;
+       p3=p2;
+  8.   cout << "p1: " << p1 << "\n";
+       cout << "*p1: " << *p1 << "\n";
+       cout << "p2: " << p2 << "\n";
+       cout << "*p2: " << *p2 << "\n";
+  9.   delete[] p2;
+       p3 = 0;
+  10.  p1 = new int[10];
+       for (int i = 0; i < 10; ++i)
+          p1[i] = pow(2, i); 
+  11.  p2 = new int[10];
+*/
+}  
+         
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+          
